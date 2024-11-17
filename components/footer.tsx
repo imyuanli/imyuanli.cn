b154aaf5789bda8306652473cfb61baba13d51dd
yuanli <2865437316@qq.com>
2024-11-18 01:45:19 +0800

    fix: 修改footer的ui

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,9 +7,13 @@ const Footer = () => {
   return (
     <footer className={'w-full'}>
       <Separator />
-      <div className={'container flex justify-between items-center py-4'}>
-        <div>
-          © {new Date()?.getFullYear()} imyuanli, Inc. All rights reserved.{' '}
+      <div
+        className={
+          'container py-4 flex flex-col justify-center items-center md:flex-row md:justify-between'
+        }
+      >
+        <div className={'mb-4 md:mb-0'}>
+          © {new Date()?.getFullYear()} imyuanli. All rights reserved.
         </div>
         <Socialize />
       </div>
