aaba39ea8f8cfa74fcdfc23cdebff5e49b57ced1
imyuanli <2865437316@qq.com>
2025-03-18 12:38:31 +0800

    feat: add ICP备案 link and adjust layout in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,7 @@
 
 import { Separator } from '@/components/ui/separator'
 import Socialize from '@/components/socialize'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
@@ -9,12 +10,13 @@ const Footer = () => {
       <Separator />
       <div
         className={
-          'container py-4 flex flex-col justify-center items-center md:flex-row md:justify-between'
+          'container py-4 flex flex-col justify-center items-center md:flex-row md:justify-between space-y-4 md:space-y-0'
         }
       >
-        <div className={'mb-4 md:mb-0'}>
+        <div>
           © {new Date()?.getFullYear()} imyuanli. All rights reserved.
         </div>
+        <a className={'text-xs'} href="https://beian.miit.gov.cn/" target="_blank">冀ICP备2020030880号-1</a>
         <Socialize />
       </div>
     </footer>
